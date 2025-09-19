6c2c8d30cacebf1a16b1a74864fdbc930abfd33d
aida zhumiyeva <128605556+AidaZZ@users.noreply.github.com>
2025-09-19 16:46:20 +0200

    GUI

--- src/Benchmarking_App/src/main.py
+++ src/Benchmarking_App/src/main.py
@@ -1,21 +1,15 @@
-from connector import load_model
-from components.camera import run_camera_loop
+# ==== main.py ====
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from windows.home_window import HomeWindow
 
 
 def main():
-    print("Choose model:")
-    print("1 - ArcFace")
-    print("2 - FaceNet")
-    print("3 - MagFace")
-
-    choice = input("Enter choice [1/2/3]: ").strip()
-    mapping = {"1": "arcface", "2": "facenet", "3": "magface"}
-    model_name = mapping.get(choice, "arcface")
-
-    wrapper = load_model(model_name)
-    print(f"[INFO] Loaded model: {wrapper.name}")
-
-    run_camera_loop(wrapper)
+    app = QApplication(sys.argv)
+    window = HomeWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
