4db190f215960073ee8dec01c517ddd2c7210cc9
AidaFriday <zhumiyevaaida@gmail.com>
2025-11-19 14:41:35 +0100

    roc_cm

--- src/benchmark_parameters/validation_accuracy/video/logic_confusion_ytf_pairs.py
+++ src/benchmark_parameters/validation_accuracy/video/logic_confusion_ytf_pairs.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 
-FIXED_THRESHOLD = 0.40
+FIXED_THRESHOLD = 0.39875417947769165
 
 
 def load_all_scores_labels(exports_dir: Path, model: str, stamp: str):
